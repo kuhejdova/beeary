0ed5535ba132082eee8a964359a19e98fea749da
Eva Kuhejdova <xkuhejd@fi.muni.cz>
2021-05-04 22:11:47 +0200

    trycatch

--- server/app.py
+++ server/app.py
@@ -169,5 +169,8 @@ def get_hive_graph():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(port=port)
+    try:
+        port = int(os.environ.get("PORT", 5000))
+        app.run(port=port)
+    except Exception as e:
+        print(e)
