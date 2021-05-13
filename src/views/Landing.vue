28297381d7b9004dda6dff4fe325686c1d5b72a9
Eva Kuhejdova <xkuhejd@fi.muni.cz>
2021-05-13 12:35:27 +0200

    fix landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,53 +1,61 @@
 <template>
+<div class="landing-wrapper">
   <div class="container">
     <div>
-      <img src="../../public/images/logo_text.svg" alt="Beeary" class="logo" />
-    </div>
+      <div class="logo-wrapper">
+        <img
+          src="../../public/images/logo_text.svg"
+          alt="Beeary"
+          class="logo"
+        />
+      </div>
 
-    <div class="logo-subtitle">
-      <p>Webová aplikace pro všechny včelaře. Ovládněte včelařství.</p>
-    </div>
-    <div class="pictograms">
-      <div class="pictogram1">
-        <object
-          id="svgSetColor"
-          type="image/svg+xml"
-          :data="svgHives"
-          v-on:load="loadSetColor"
-        >
-          <img src="../../public/images/hives.svg" alt="Beeary" />
-        </object>
-        <p>Přehled všech úlů</p>
+      <div class="logo-subtitle">
+        <p>Webová aplikace pro všechny včelaře. Ovládněte včelařství.</p>
       </div>
-      <div class="pictogram2">
-        <object
-          id="svgSetColor"
-          type="image/svg+xml"
-          :data="svgTimeline"
-          v-on:load="loadSetColor"
-        >
-          <img src="../../public/images/timeline.svg" alt="Beeary" />
-        </object>
-        <p>Časová osa s poznámkami</p>
-        <!-- <img src="../../public/images/timeline.svg" alt="Beeary" /> -->
+      <div class="pictograms">
+        <div class="pictogram1">
+          <object
+            id="svgSetColor"
+            type="image/svg+xml"
+            :data="svgHives"
+            v-on:load="loadSetColor"
+          >
+            <img src="../../public/images/hives.svg" alt="Beeary" />
+          </object>
+          <p>Přehled všech úlů</p>
+        </div>
+        <div class="pictogram2">
+          <object
+            id="svgSetColor"
+            type="image/svg+xml"
+            :data="svgTimeline"
+            v-on:load="loadSetColor"
+          >
+            <img src="../../public/images/timeline.svg" alt="Beeary" />
+          </object>
+          <p>Časová osa s poznámkami</p>
+          <!-- <img src="../../public/images/timeline.svg" alt="Beeary" /> -->
+        </div>
+        <div class="pictogram3">
+          <object
+            id="svgSetColor"
+            type="image/svg+xml"
+            :data="svgMonitoring"
+            v-on:load="loadSetColor"
+          >
+            <img src="../../public/images/monitoring.svg" alt="Beeary" />
+          </object>
+          <p>Celoroční monitoring</p>
+          <!-- <img src="../../public/images/monitoring.svg" alt="Beeary" /> -->
+        </div>
       </div>
-      <div class="pictogram3">
-        <object
-          id="svgSetColor"
-          type="image/svg+xml"
-          :data="svgMonitoring"
-          v-on:load="loadSetColor"
-        >
-          <img src="../../public/images/monitoring.svg" alt="Beeary" />
-        </object>
-        <p>Celoroční monitoring</p>
-        <!-- <img src="../../public/images/monitoring.svg" alt="Beeary" /> -->
+      <div class="buttonwrapper">
+        <button @click="redirectHome" class="button">Pojď začít</button>
       </div>
-    </div>
-    <div class="buttonwrapper">
-      <button @click="redirectHome" class="button">Pojď začít</button>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -82,10 +90,21 @@ export default {
 };
 </script>
 
+
+
 <style scoped>
-* {
+/* * {
   margin: 0;
+} */
+
+.landing-wrapper{
+  height: calc(100% + 60px);
+  background-image: url(../../public/images/background_pattern_missing.svg) ;
+  background-size: cover;
+  background-repeat: space;
+  background-position: center;
 }
+
 
 .pictograms {
   display: flex;
@@ -111,7 +130,7 @@ export default {
 }
 
 .pictograms > div > object {
-  height: 300px;
+  height: 200px;
 }
 
 .buttonwrapper {
@@ -120,35 +139,49 @@ export default {
   margin-bottom: 10px;
 }
 
-.container,
+/* .container,
 section {
-}
+} */
 
 .container {
   display: flex;
   flex-direction: column;
-  /* justify-content: space-evenly; */
-  justify-content: center;
-  width: 100%;
-  /* height: calc(100% + 60px);
-  background-image: url(../../public/images/background_pattern_missing.svg) ;
+  justify-content: space-evenly;
+  /* justify-content: flex-start; */
+  height: 100%;
+  position: relative;
+  left: 0px;
+  right: 0px;
+  margin: auto;
+  top: 0px;
+  bottom: 0px;
+  height: 100%;
+  /* background-image: url(../../public/images/background_pattern_missing.svg) ;
   background-size: cover;
   background-repeat: space;
   background-position: center; */
   /* margin-bottom: 10px; */
 }
 
-.container > div {
+.container > div > div {
   /* background-color: rgba(255, 255, 255, 0.8); */
   align-self: center;
   min-width: 250px;
 }
+.container > div > div:not(:first-child) {
+  margin-top: 10px;
+}
 
 .logo {
-  height: 100px;
-  width: 200px;
+  height: 125px;
+  width: 250px;
+  /* margin-top: 0px; */
   /* height: min(30vw, 100%);
     width: 90vw; */
+}
+
+#svgSetColor {
+  height: 30vh;
 }
 
 object {
@@ -156,32 +189,6 @@ object {
   width: 100%;
   /* height: min(30vw, 100%);
     width: 50vw; */
-}
-
-@media (min-width: 690px) {
-  /* img {
-    height: 100%;
-    width: 70%;
-    /* height: min(30vw, 100%);
-    width: min(30vw, 100%); 
-  } */
-  .pictograms {
-    width: 100%;
-    overflow-x: auto;
-    justify-content: space-evenly;
-  }
-
-  .pictograms > div {
-    width: auto;
-    flex: 0 1 auto;
-  }
-
-  .logo {
-  height: 200px;
-  width: 400px;
-  /* height: min(30vw, 100%);
-    width: 90vw; */
-}
 }
 
 .pictogram1,
@@ -222,16 +229,54 @@ p {
   color: #7d9098;
 }
 
-p b {
+/* p b {
   color: #ffffff;
-  display: inline-block;
-  padding: 5px 10px;
-  background-color: #c4d7e0;
+  display: inline-block; */
+  /* padding: 5px 10px; */
+  /* background-color: #c4d7e0;
   border-radius: 2px;
   text-transform: uppercase;
   font-size: 18px;
-}
-.resource {
+} */
+/* .resource {
   margin: 20px 0;
+} */
+
+@media (min-width: 690px) {
+  .container {
+    width: 80%;
+  }
+  /* img {
+    height: 100%;
+    width: 70%;
+    /* height: min(30vw, 100%);
+    width: min(30vw, 100%); 
+  } */
+  .pictograms {
+    width: 100%;
+    overflow-x: auto;
+    justify-content: space-evenly;
+  }
+
+  .pictograms > div {
+    width: auto;
+    flex: 0 1 auto;
+  }
+  .pictograms > div > object {
+    height: 300px;
+  }
+
+  .logo {
+    min-height: 200px;
+    min-width: 400px;
+    height: 20vh;
+    width: 25vw;
+    /* height: min(30vw, 100%);
+    width: 90vw; */
+  }
+
+  p {
+    font-size: 1.8em;
+  }
 }
 </style>
